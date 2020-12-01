3ce88dfac53b1d5d1667179688c0e653d40d313e
Andreas Andersson <andreas@herrandersson.se>
2020-12-01 20:57:43 +0100

    day1: Solve part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -12,11 +12,33 @@ fn find_terms2(terms: &[i32], sum: i32) -> Option<(i32, i32)> {
     })
 }
 
+fn find_terms3(terms: &[i32], sum: i32) -> Option<(i32, i32, i32)> {
+    terms.iter().enumerate().find_map(|(i1, t1)| {
+        if let Some((t2, t3)) = find_terms2(&terms[i1 + 1..], sum - t1) {
+            Some((*t1, t2, t3))
+        } else {
+            None
+        }
+    })
+}
+
 fn solve_part1(terms: &[i32], sum: i32) {
     match find_terms2(&terms[..], sum) {
         Some((t1, t2)) => {
             println!("{} + {} = {}", t1, t2, t1 + t2);
             println!("{} * {} = {}", t1, t2, t1 * t2);
+        }
+        None => {
+            println!("Failed to find terms that add up to {}", sum);
+        }
+    }
+}
+
+fn solve_part2(terms: &[i32], sum: i32) {
+    match find_terms3(&terms[..], sum) {
+        Some((t1, t2, t3)) => {
+            println!("{} + {} + {} = {}", t1, t2, t3, t1 + t2 + t3);
+            println!("{} * {} * {} = {}", t1, t2, t3, t1 * t2 * t3);
         }
         None => {
             println!("Failed to find terms that add up to {}", sum);
@@ -36,6 +58,7 @@ fn main() {
         .map(|s| s.parse::<i32>().expect("Failed to parse i32"))
         .collect();
     solve_part1(&terms[..], 2020);
+    solve_part2(&terms[..], 2020);
 }
 
 #[cfg(test)]
@@ -47,5 +70,10 @@ mod tests {
     #[test]
     fn test_example_two_terms() {
         assert_eq!(find_terms2(&EXAMPLE_TERMS, 2020), Some((1721, 299)));
+    }
+
+    #[test]
+    fn test_example_three_terms() {
+        assert_eq!(find_terms3(&EXAMPLE_TERMS, 2020), Some((979, 366, 675)));
     }
 }
