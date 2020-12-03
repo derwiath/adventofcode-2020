cb668eff94480c4ada71f17ed9170dfa9be0458f
Andreas Andersson <andreas@herrandersson.se>
2020-12-03 06:38:26 +0100

    day3: Solve part 1

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -5,30 +5,88 @@ extern crate regex;
 use std::env;
 use std::fs;
 
+struct Pos {
+    x: usize,
+    y: usize,
+}
+
+impl Pos {
+    fn new(x: usize, y: usize) -> Self {
+        Self { x, y }
+    }
+}
+
+fn count_trees(map: &str, slope: &Pos) -> u32 {
+    lazy_static! {
+        static ref MAP_RE: regex::Regex = regex::Regex::new(r"\([\.#]*^\)").unwrap();
+    }
+
+    let rows: Vec<&str> = map
+        .lines()
+        .filter(|r| r.len() > 0)
+        .map(|r| {
+            if r.ends_with("  --->") {
+                &r[0..r.len() - "  --->".len()]
+            } else {
+                &r[..]
+            }
+        })
+        .collect();
+    let mut trees = 0;
+    let mut pos = Pos::new(0, 0);
+    let width = rows.first().unwrap().len();
+    while pos.y < rows.len() {
+        pos.x = (pos.x + slope.x) % width;
+        pos.y += slope.y;
+        let tree = if let Some(row) = rows.get(pos.y) {
+            if let Some(c) = row.chars().nth(pos.x) {
+                c == '#'
+            } else {
+                false
+            }
+        } else {
+            false
+        };
+        if tree {
+            trees += 1;
+        }
+
+        println!("{} {}: tree {}, count {}", pos.x, pos.y, tree, trees);
+    }
+    trees
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = args.get(1).expect("Usage: day1 input-filename");
-    lazy_static! {
-        static ref PARSE_RE: regex::Regex =
-            regex::Regex::new(r"(\d*)-(\d*) ([a-z]): (.*)").unwrap();
-            //regex::Regex::new(r"\(\d*\)-\(\d*\) \([a-z]\): \(.*\)").unwrap();
-    }
 
     println!("Reading input from {}", filename);
     let input = fs::read_to_string(filename).expect("Failed to read file");
 
-    for line in input.lines() {
-        println!("{}, {}", line, PARSE_RE.is_match(line));
-    }
+    let slope = Pos::new(3, 1);
+    println!("Trees {}", count_trees(&input, &slope));
 }
 
 #[cfg(test)]
 mod tests3 {
     use super::*;
 
+    const EXAMPLE: &str = r"
+..##.........##.........##.........##.........##.........##.......  --->
+#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+.#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+.#...##..#..#...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+..#.##.......#.##.......#.##.......#.##.......#.##.......#.##.....  --->
+.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+.#........#.#........#.#........#.#........#.#........#.#........#
+#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...
+#...##....##...##....##...##....##...##....##...##....##...##....#
+.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+";
     #[test]
     fn test_example() {
-        const EXAMPLE: &str = r"";
-        assert_eq!(EXAMPLE.len(), 0);
+        let slope = Pos::new(3, 1);
+        assert_eq!(count_trees(EXAMPLE, &slope), 7);
     }
 }
