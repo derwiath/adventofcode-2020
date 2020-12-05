3383cf30a810d25549b4065876fc8c5bd597e8ed
Andreas Andersson <andreas@herrandersson.se>
2020-12-05 07:26:15 +0100

    day5: Solve part 2

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -17,6 +17,19 @@ fn calc_seat_id(s: &str) -> usize {
     id
 }
 
+fn find_first_missing_seat(seat_ids: &[usize]) -> Option<usize> {
+    let mut last = None;
+    for seat_id in seat_ids.iter() {
+        if let Some(l) = last {
+            if l != seat_id - 1 {
+                return Some(seat_id - 1);
+            }
+        }
+        last = Some(*seat_id);
+    }
+    None
+}
+
 fn main() {
     lazy_static! {
         static ref RE: regex::Regex = regex::Regex::new(r"hello").unwrap();
@@ -26,12 +39,18 @@ fn main() {
     println!("Reading input from {}", filename);
     let input = fs::read_to_string(filename).expect("Failed to read file");
     let mut max_id = 0;
+    let mut seat_ids: Vec<usize> = Vec::new();
     for line in input.lines() {
         let seat_id = calc_seat_id(line);
         max_id = cmp::max(seat_id, max_id);
+        seat_ids.push(seat_id);
     }
+    seat_ids.sort();
+
+    let my_seat_id = find_first_missing_seat(&seat_ids);
 
     println!("Max seat id {}", max_id);
+    println!("My seat id {}", my_seat_id.unwrap());
 }
 
 #[cfg(test)]
