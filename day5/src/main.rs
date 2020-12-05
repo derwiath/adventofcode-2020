03fa2f19abeba776d2ddf3f0d97bcf3bb3826647
Andreas Andersson <andreas@herrandersson.se>
2020-12-05 07:24:37 +0100

    day5: Solve part 1

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -2,8 +2,20 @@
 extern crate lazy_static;
 extern crate regex;
 
+use std::cmp;
 use std::env;
 use std::fs;
+
+fn calc_seat_id(s: &str) -> usize {
+    let mut id: usize = 0;
+    for c in s.chars() {
+        id = id << 1;
+        if c == 'B' || c == 'R' {
+            id = id | 0x1
+        }
+    }
+    id
+}
 
 fn main() {
     lazy_static! {
@@ -12,15 +24,31 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = args.get(1).expect("Usage: day4 input-filename");
     println!("Reading input from {}", filename);
+    let input = fs::read_to_string(filename).expect("Failed to read file");
+    let mut max_id = 0;
+    for line in input.lines() {
+        let seat_id = calc_seat_id(line);
+        max_id = cmp::max(seat_id, max_id);
+    }
+
+    println!("Max seat id {}", max_id);
 }
 
 #[cfg(test)]
 mod tests5 {
     use super::*;
 
-    const EXAMPLE: &str = "";
+    const EXAMPLES: [(&str, usize); 4] = [
+        ("FBFBBFFRLR", 357),
+        ("BFFFBBFRRR", 567),
+        ("FFFBBBFRRR", 119),
+        ("BBFFBBFRLL", 820),
+    ];
+
     #[test]
     fn test_1() {
-        assert_eq!(1 + 1, 2);
+        for (example, seat_id) in EXAMPLES.iter() {
+            assert_eq!(calc_seat_id(example), *seat_id);
+        }
     }
 }
