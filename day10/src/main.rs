7e5130fa202973bf8e874a045fe6347ee699dcca
Andreas Andersson <andreas@herrandersson.se>
2020-12-10 06:47:23 +0100

    day10: Solve part 1

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,29 +1,48 @@
-#[macro_use]
-extern crate lazy_static;
-extern crate regex;
-
 use std::env;
 use std::fs;
 
-fn solve_part1(input: &str) -> usize {
-    lazy_static! {
-        static ref RE: regex::Regex = regex::Regex::new(r"(\d*) ([a-z]*)").unwrap();
-    }
-    let mut sum = 0;
-    for line in input.lines() {
-        if let Some(captures) = RE.captures(line) {
-            if captures.len() == 3 {
-                let count = captures.get(1).unwrap().as_str().parse::<usize>().unwrap();
-                let _thing = captures.get(2).unwrap().as_str();
-                sum += count;
-            }
+fn parse_adapters(input: &str) -> Vec<u64> {
+    let mut adapters: Vec<u64> = input.lines().map(|l| l.parse::<u64>().unwrap()).collect();
+    adapters.sort_unstable();
+    let max_adapter = adapters[adapters.len() - 1];
+    adapters.push(max_adapter + 3);
+    adapters
+}
+/*
+1
+4
+5
+6
+7
+10
+11
+12
+15
+16
+19
+*/
+fn solve_part1(input: &str) -> u64 {
+    let adapters = parse_adapters(input);
+    let mut jolt_counts: [u64; 3] = [0, 0, 0];
+    let mut outlet: u64 = 0;
+    for adapter in adapters {
+        let diff = adapter - outlet;
+        if diff > 3 {
+            panic!("adapter {} out of range, outlet {}", adapter, outlet);
         }
+        jolt_counts[(diff - 1) as usize] += 1;
+        println!(
+            "adapter {:3}, outlet {:3}, diff {:1}, jolt_counts {:3} {:3}",
+            adapter, outlet, diff, jolt_counts[0], jolt_counts[2]
+        );
+        outlet = adapter;
     }
-    sum
+
+    jolt_counts[0] * jolt_counts[2]
 }
 
-fn solve_part2(input: &str) -> usize {
-    input.len()
+fn solve_part2(_input: &str) -> u64 {
+    0
 }
 
 fn main() {
@@ -44,16 +63,60 @@ fn main() {
 mod tests10 {
     use super::*;
 
-    const EXAMPLE1: &str = "
-3 seals
-4 quacks";
-
+    const EXAMPLE1: &str = "16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4
+";
     #[test]
     fn test1_1() {
-        assert_eq!(solve_part1(EXAMPLE1), 7);
+        assert_eq!(solve_part1(EXAMPLE1), 7 * 5);
     }
 
-    const EXAMPLE2: &str = "";
+    const EXAMPLE2: &str = "28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3
+";
+
+    #[test]
+    fn test1_2() {
+        assert_eq!(solve_part1(EXAMPLE2), 22 * 10);
+    }
 
     #[test]
     fn test2_1() {
