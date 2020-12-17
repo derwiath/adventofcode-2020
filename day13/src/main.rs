2948670f4fd5ac06cad68d160dc098fbe34bfe7f
Andreas Andersson <andreas@herrandersson.se>
2020-12-17 22:45:24 +0100

    day13: Solve part1

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,29 +1,26 @@
-#[macro_use]
-extern crate lazy_static;
-extern crate regex;
-
 use std::env;
 use std::fs;
 
-fn solve_part1(input: &str) -> usize {
-    lazy_static! {
-        static ref RE: regex::Regex = regex::Regex::new(r"(\d*) ([a-z]*)").unwrap();
-    }
-    let mut sum = 0;
-    for line in input.lines() {
-        if let Some(captures) = RE.captures(line) {
-            if captures.len() == 3 {
-                let count = captures.get(1).unwrap().as_str().parse::<usize>().unwrap();
-                let _thing = captures.get(2).unwrap().as_str();
-                sum += count;
-            }
-        }
-    }
-    sum
+fn solve_part1(input: &str) -> Option<u64> {
+    let time = input.lines().nth(0).unwrap().parse::<u64>().unwrap();
+    let (bus_id, wait_time) = input
+        .lines()
+        .nth(1)
+        .unwrap()
+        .split(",")
+        .filter(|s| s != &"x")
+        .map(|s| s.parse::<u64>().unwrap())
+        .map(|bus_id| (bus_id, bus_id - &time % bus_id))
+        .min_by_key(|(_, wait_time)| *wait_time)
+        .unwrap();
+    println!("time {}", time);
+    println!("bus_id {}", bus_id);
+    println!("wait_time{}", wait_time);
+    Some(bus_id * wait_time)
 }
 
-fn solve_part2(input: &str) -> usize {
-    input.len()
+fn solve_part2(_input: &str) -> Option<u64> {
+    None
 }
 
 fn main() {
@@ -34,29 +31,29 @@ fn main() {
     let input = fs::read_to_string(filename).expect("Failed to read file");
 
     let answer1 = solve_part1(&input);
-    println!("Answer 1: {}", answer1);
+    println!("Answer 1: {:?}", answer1);
 
     let answer2 = solve_part2(&input);
-    println!("Answer 2: {}", answer2);
+    println!("Answer 2: {:?}", answer2);
 }
 
 #[cfg(test)]
 mod tests13 {
     use super::*;
 
-    const EXAMPLE1: &str = "
-3 seals
-4 quacks";
+    const EXAMPLE1: &str = "\
+            939\n\
+            7,13,x,x,59,x,31,19";
 
     #[test]
     fn test1_1() {
-        assert_eq!(solve_part1(EXAMPLE1), 7);
+        assert_eq!(solve_part1(EXAMPLE1), Some(295));
     }
 
     const EXAMPLE2: &str = "";
 
     #[test]
     fn test2_1() {
-        assert_eq!(solve_part2(EXAMPLE2), 0);
+        assert_eq!(solve_part2(EXAMPLE2), None);
     }
 }
