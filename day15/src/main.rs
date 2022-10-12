035919c5939fd64f7e11af13305e1a6b5ea37287
Andreas Andersson <andreas@neoboid.com>
2022-10-12 07:29:20 +0200

    day15: Solve part 1

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,25 +1,66 @@
-#[macro_use]
-extern crate lazy_static;
-extern crate regex;
-
+use std::collections::HashMap;
 use std::env;
 use std::fs;
 
-fn solve_part1(input: &str, _: u32) -> usize {
-    lazy_static! {
-        static ref RE: regex::Regex = regex::Regex::new(r"(\d*) ([a-z]*)").unwrap();
-    }
-    let mut sum = 0;
-    for line in input.lines() {
-        if let Some(captures) = RE.captures(line) {
-            if captures.len() == 3 {
-                let count = captures.get(1).unwrap().as_str().parse::<usize>().unwrap();
-                let _thing = captures.get(2).unwrap().as_str();
-                sum += count;
-            }
+struct Info {
+    last_turn_seen: usize,
+    next_last_turn_seen: usize,
+}
+
+impl Info {
+    fn new(last_turn_seen: usize, next_last_turn_seen: usize) -> Info {
+        Info {
+            last_turn_seen,
+            next_last_turn_seen,
         }
     }
-    sum
+}
+
+fn solve_part1(input: &str, turns: usize) -> usize {
+    let input_numbers = input.split(",");
+    let mut number_to_turn = HashMap::new();
+    let mut turn: usize = 1;
+    let mut last_number = 0;
+    for input_number in input_numbers {
+        println!("({})", input_number);
+        let number = input_number.trim_end().parse::<usize>().unwrap();
+        number_to_turn.insert(number, Info::new(turn, 0));
+        println!("{}: {} -> ({}, {})", turn, number, turn, 0);
+        last_number = number;
+        turn += 1;
+    }
+
+    while turn <= turns {
+        let number = if let Some(last_info) = number_to_turn.get(&last_number) {
+            println!(
+                "{}: {} -> ({}, {}) (seen)",
+                turn, last_number, last_info.last_turn_seen, last_info.next_last_turn_seen
+            );
+            if last_info.next_last_turn_seen > 0 {
+                last_info.last_turn_seen - last_info.next_last_turn_seen
+            } else {
+                0
+            }
+        } else {
+            println!("{}: {} -> ({}, {}) (not seen)", turn, 0, turn, 0);
+            0
+        };
+        let next_last_turn_seen = if let Some(info) = number_to_turn.get(&number) {
+            info.last_turn_seen
+        } else {
+            0
+        };
+        let info = Info::new(turn, next_last_turn_seen);
+        println!(
+            "{}: {} -> ({}, {})",
+            turn, number, turn, next_last_turn_seen
+        );
+        number_to_turn.insert(number, info);
+        last_number = number;
+        turn += 1;
+    }
+
+    return last_number;
 }
 
 fn main() {
@@ -39,6 +80,20 @@ fn main() {
 #[cfg(test)]
 mod tests15 {
     use super::*;
+
+    #[test]
+    fn test1_example_10() {
+        assert_eq!(solve_part1("0,3,6", 10), 0);
+    }
+
+    #[test]
+    fn test1_example_4() {
+        assert_eq!(solve_part1("0,3,6", 4), 0);
+    }
+    #[test]
+    fn test1_example_5() {
+        assert_eq!(solve_part1("0,3,6", 5), 3);
+    }
 
     #[test]
     fn test1_0() {
