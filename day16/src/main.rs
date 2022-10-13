87f6a5e588d5c08cebdf0e400307d5a5d916a192
Andreas Andersson <andreas@neoboid.com>
2022-10-13 08:16:22 +0200

    day16: Solve day1

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -5,21 +5,150 @@ extern crate regex;
 use std::env;
 use std::fs;
 
-fn solve_part1(input: &str) -> usize {
-    lazy_static! {
-        static ref RE: regex::Regex = regex::Regex::new(r"(\d*) ([a-z]*)").unwrap();
+// Inclusive range
+#[derive(Debug, PartialEq)]
+struct Range {
+    min: usize,
+    max: usize,
+}
+
+impl Range {
+    fn new(min: usize, max: usize) -> Range {
+        Range { min, max }
     }
-    let mut sum = 0;
-    for line in input.lines() {
-        if let Some(captures) = RE.captures(line) {
-            if captures.len() == 3 {
-                let count = captures.get(1).unwrap().as_str().parse::<usize>().unwrap();
-                let _thing = captures.get(2).unwrap().as_str();
-                sum += count;
-            }
+
+    #[allow(dead_code)]
+    fn union(&self, other: &Range) -> Option<Range> {
+        if self.includes(&other.min) || self.includes(&other.max) {
+            Some(Range::new(self.min.min(other.min), self.max.max(other.max)))
+        } else {
+            None
         }
     }
-    sum
+
+    fn includes(&self, number: &usize) -> bool {
+        self.min <= *number && *number <= self.max
+    }
+}
+
+#[derive(Debug)]
+enum ParseState {
+    Rules,
+    YourTicket,
+    NearbyTickets,
+    Eof,
+}
+
+#[allow(dead_code)]
+struct TicketInfo {
+    ranges: Vec<Range>,
+    your_numbers: Vec<usize>,
+    nearby_numbers: Vec<usize>,
+}
+
+impl TicketInfo {
+    fn new(ranges: Vec<Range>, your_numbers: Vec<usize>, nearby_numbers: Vec<usize>) -> TicketInfo {
+        TicketInfo {
+            ranges,
+            your_numbers,
+            nearby_numbers,
+        }
+    }
+}
+
+fn parse_rule(line: &str) -> Result<(Range, Range), ()> {
+    lazy_static! {
+        static ref RULES_RE: regex::Regex =
+            regex::Regex::new(r".*: ([\d]*)-([\d]*) or ([\d]*)-([\d]*)").unwrap();
+    }
+    if let Some(captures) = RULES_RE.captures(line) {
+        assert_eq!(captures.len(), 5);
+        let numbers: Vec<usize> = captures
+            .iter()
+            .skip(1)
+            .map(|s| s.unwrap().as_str().parse::<usize>().unwrap())
+            .collect();
+        Ok((
+            (Range::new(numbers[0], numbers[1])),
+            Range::new(numbers[2], numbers[3]),
+        ))
+    } else {
+        Err(())
+    }
+}
+
+fn parse_ticket_info(input: &str) -> Result<TicketInfo, &'static str> {
+    let mut parse_state = ParseState::Rules;
+    let mut ranges = Vec::<Range>::new();
+    let mut your_numbers = Vec::<usize>::new();
+    let mut nearby_numbers = Vec::<usize>::new();
+    for line in input.lines().skip_while(|l| l.len() == 0) {
+        let next_parse_state: ParseState = match parse_state {
+            ParseState::Rules => {
+                if line.len() == 0 {
+                    ParseState::YourTicket
+                } else if let Ok((range1, range2)) = parse_rule(line) {
+                    ranges.push(range1);
+                    ranges.push(range2);
+                    ParseState::Rules
+                } else {
+                    return Err("Failed to parse rule");
+                }
+            }
+            ParseState::YourTicket => {
+                if line == "your ticket:" {
+                    ParseState::YourTicket
+                } else if line.len() == 0 {
+                    ParseState::NearbyTickets
+                } else {
+                    line.split(',')
+                        .map(|s| s.parse::<usize>().unwrap())
+                        .for_each(|n| your_numbers.push(n));
+                    ParseState::NearbyTickets
+                }
+            }
+            ParseState::NearbyTickets => {
+                if line == "nearby tickets:" {
+                    ParseState::NearbyTickets
+                } else if line.len() == 0 {
+                    if nearby_numbers.len() > 0 {
+                        ParseState::Eof
+                    } else {
+                        ParseState::NearbyTickets
+                    }
+                } else {
+                    line.split(',')
+                        .map(|s| (s.parse::<usize>().unwrap()))
+                        .for_each(|n| nearby_numbers.push(n));
+                    ParseState::NearbyTickets
+                }
+            }
+            ParseState::Eof => {
+                break;
+            }
+        };
+        parse_state = next_parse_state
+    }
+    Ok(TicketInfo::new(ranges, your_numbers, nearby_numbers))
+}
+
+fn solve_part1(input: &str) -> usize {
+    let ticket_info = match parse_ticket_info(input) {
+        Ok(ticket_info) => ticket_info,
+        Err(msg) => panic!("Error: {}", msg),
+    };
+
+    ticket_info
+        .nearby_numbers
+        .iter()
+        .filter(|number| {
+            ticket_info
+                .ranges
+                .iter()
+                .find(|r| r.includes(number))
+                .is_none()
+        })
+        .fold(0, |acc, invalid_number| acc + invalid_number)
 }
 
 fn solve_part2(input: &str) -> usize {
@@ -47,13 +176,32 @@ fn main() {
 mod tests16 {
     use super::*;
 
+    #[test]
+    fn test1_parserule() {
+        assert_eq!(
+            parse_rule("class: 1-3 or 5-7"),
+            Ok((Range::new(1, 3), Range::new(5, 7)))
+        );
+    }
+
     const EXAMPLE1: &str = "
-3 seals
-4 quacks";
+class: 1-3 or 5-7
+row: 6-11 or 33-44
+seat: 13-40 or 45-50
+
+your ticket:
+7,1,14
+
+nearby tickets:
+7,3,47
+40,4,50
+55,2,20
+38,6,12
+";
 
     #[test]
     fn test1_1() {
-        assert_eq!(solve_part1(EXAMPLE1), 7);
+        assert_eq!(solve_part1(EXAMPLE1), 71);
     }
 
     const EXAMPLE2: &str = "";
